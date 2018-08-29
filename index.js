db0856b6ec657c6af9ae876d2437b57046ef3576
morthos <pathshan61@gmail.com>
2018-08-29 11:22:13 -0400

    .

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+    
+var boxTop = 200;
+var boxLeft = 200;
+var rightPressed = false;
+var leftPressed = false;
+var downPressed = false;
+var upPressed = false;
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    switch(keyName){
+        case "ArrowDown":
+            console.log(keyName + " pressed")
+            downPressed = true
+            boxTop += 10
+            document.getElementById("box").style.top = boxTop + "px"
+            break
+        case "ArrowUp":
+            console.log(keyName + " pressed")
+            upPressed = true
+            boxTop -= 10
+            document.getElementById("box").style.top = boxTop + "px"
+            break
+            
+        case "ArrowLeft": 
+            console.log(keyName + " pressed")
+            leftPressed = true
+            boxLeft += 10
+            document.getElementById("box").style.left  = boxLeft + "px"
+            break
+        case "ArrowRight":
+            console.log(keyName + " pressed")
+            rightPressed = true
+            boxLeft += 10
+            document.getElementById("box").style.left  = boxLeft + "px"
+            break
+    }
+});
+// document.addEventListener('keyup', (event) => {
+//     let top = document.getElementById("box").style.top
+//     let left = document.getElementById("box").style.left 
+//     const keyName = event.key;
+//     switch(keyName){
+//         case "ArrowDown":
+//             console.log(keyName + " not pressed")
+//             downPressed = false
+//         case "ArrowUp":
+//             console.log(keyName + " not pressed")
+//             upPressed = false
+//         case "ArrowLeft": 
+//             console.log(keyName + " not pressed")
+//             leftPressed = false
+//         case "ArrowRight":
+//             console.log(keyName + " not pressed")
+//             rightPressed = false
+//     }
+// })
